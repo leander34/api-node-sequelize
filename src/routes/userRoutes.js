6ab04e3fdfef9f1eb3a67d8327e9f146880068a1
Leander Silveira Santos <leandersilveira@hotmail.com>
2022-05-13 15:50:51 -0300

    abrindo rota para criar usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,7 +7,7 @@ const routes = new Router();
 routes.get("/", userController.index);
 routes.get("/:id", userController.show);
 
-routes.post("/", loginRequired, userController.store);
+routes.post("/", userController.store);
 routes.put("/", loginRequired, userController.update);
 routes.delete("/", loginRequired, userController.delete);
 
